0dee0f92ceb5854f7a561c326983b26ccb369ddf
Joshua Immordino <joshimmor@gmail.com>
2024-01-12 15:29:27 -0500

    white

--- components/Products.js
+++ components/Products.js
@@ -5,9 +5,9 @@ export default function Products() {
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
             <Image className="w-full" src="/camera.jpg" width={50}  height={50}alt="Sunset in the mountains"/>
             <div className="px-6 py-1">
-                <div className="grid grid-cols-2 justify justify-items-start">
+                <div className="grid grid-cols-2 justify justify-items-start w-2/3">
                     <p className="text-red-700 text-base line-through text-sm"> $25.00 </p>
-                    <p className="text-gray-100 text-base text-sm"> $15.00 </p>
+                    <p className="text-gray-900 text-base text-sm"> $15.00 </p>
                 </div>
                 <div className="font-bold text-xl mb-2">FujiFilm X32</div>
             </div>
--- components/Slider.js
+++ components/Slider.js
@@ -61,7 +61,7 @@ export const SwipeCarousel = () => {
   };
 
   return (
-    <div className="relative overflow-hidden bg-neutral-950 py-3">
+    <div className="relative overflow-hidden bg-neutral-150 py-3">
       <motion.div
         drag="x"
         dragConstraints={{
@@ -82,7 +82,7 @@ export const SwipeCarousel = () => {
       </motion.div>
 
       <Dots imgIndex={imgIndex} setImgIndex={setImgIndex} />
-      <GradientEdges />
+ 
     </div>
   );
 };
@@ -141,8 +141,8 @@ const Dots = ({ imgIndex, setImgIndex }) => {
 const GradientEdges = () => {
   return (
     <>
-      <div className="pointer-events-none absolute bottom-0 left-0 top-0 w-[10vw] max-w-[100px] bg-gradient-to-r from-neutral-950/50 to-neutral-950/0" />
-      <div className="pointer-events-none absolute bottom-0 right-0 top-0 w-[10vw] max-w-[100px] bg-gradient-to-l from-neutral-950/50 to-neutral-950/0" />
+      <div className="pointer-events-none absolute bottom-0 left-0 top-0 w-[10vw] max-w-[100px] bg-gradient-to-r from-neutral-950/50 to-neutral-150/0" />
+      <div className="pointer-events-none absolute bottom-0 right-0 top-0 w-[10vw] max-w-[100px] bg-gradient-to-l from-neutral-950/50 to-neutral-150/0" />
     </>
   );
 };
