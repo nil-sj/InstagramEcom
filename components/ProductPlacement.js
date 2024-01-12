7027d080da6cd6210a72f572cf6d98aa712ae48f
Joshua Immordino <joshimmor@gmail.com>
2024-01-12 14:48:29 -0500

    first commit

---
+++ components/ProductPlacement.js
@@ -0,0 +1,20 @@
+import Products from "./Products"
+
+export default function ProductPlacement() {
+  return (
+    <div className="grid grid-cols-2 gap-2 p-2">
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+    </div>
+  )
+}
---
+++ components/Products.js
@@ -0,0 +1,19 @@
+
+import Image from 'next/image'
+export default function Products() {
+  return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <Image className="w-full" src="/camera.jpg" width={50}  height={50}alt="Sunset in the mountains"/>
+            <div className="px-6 py-1">
+                <div className="grid grid-cols-2 justify justify-items-start">
+                    <p className="text-red-700 text-base line-through"> $25.00 </p>
+                    <p className="text-gray-100 text-base"> $15.00 </p>
+                </div>
+                <div className="font-bold text-xl mb-2">FujiFilm X32</div>
+            </div>
+            <div className="grid grid-cols-1 px-1 pt-0 pb-2 justify justify-items-end">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">Amazon</button>
+            </div>
+        </div>
+  )
+}
